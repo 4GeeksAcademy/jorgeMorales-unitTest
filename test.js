c518cc2febad347ee09f79cfbd3bd20c0f7c699b
Jorge Morales <85179035+Jm9710@users.noreply.github.com>
2024-10-16 13:35:45 +0000

    correcion realizada

--- test.js
+++ test.js
@@ -11,3 +11,30 @@ test("One euro should be 1.07 dollars", function() {
     // Hago mi comparación (la prueba)
     expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
 })
+
+
+test("One dolar should be 156.5 yen", function(){
+
+    const { fromDollarToYen } = require('./app.js');
+
+    const yen = fromDollarToYen(5);
+
+    const expected = (5 / 1.07) * 156.5;
+
+    expect(yen).toBe(expected)
+
+})
+
+test("One yen should be 0.87/156.5 pounds", function() {
+    
+    const { fromYenToPound } = require('./app.js');
+
+    
+    const pounds = fromYenToPound(1000);
+
+    
+    const expected = (1000 / 156.5) * 0.87;
+
+    
+    expect(pounds).toBe(expected);
+})
